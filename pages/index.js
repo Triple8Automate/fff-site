41af2f8555bc47890a9bf289124143101d4677a3
Triple8Automate <mark@888automate.com>
2025-10-19 10:09:11 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 import fs from "fs";
 import path from "path";
+import Head from "next/head";
+import Script from "next/script";
 
 export async function getStaticProps() {
   const htmlPath = path.join(process.cwd(), "public", "index.html");
-  let html = fs.readFileSync(htmlPath, "utf-8");
-
-  // Fix relative paths to work with Next.js public folder
-  html = html
-    .replace(/href="\.\/index-3C0zujvJ\.css"/g, 'href="/index-3C0zujvJ.css"')
-    .replace(/src="\.\/index-Ddi3IMu4\.js"/g, 'src="/index-Ddi3IMu4.js"');
-
-  return { props: { html } };
+  const full = fs.readFileSync(htmlPath, "utf-8");
+  // grab only the BODY so scripts/styles can be attached properly
+  const match = full.match(/<body[^>]*>([\s\S]*?)<\/body>/i);
+  const body = match ? match[1] : full;
+  return { props: { body } };
 }
 
-export default function Home({ html }) {
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+export default function Home({ body }) {
+  return (
+    <>
+      <Head>
+        {/* load Manus CSS from /public */}
+        <link rel="stylesheet" href="/index-3C0zujvJ.css" />
+      </Head>
+
+      {/* render body markup */}
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+
+      {/* load Manus JS so it actually executes */}
+      <Script src="/index-Ddi3IMu4.js" strategy="afterInteractive" />
+    </>
+  );
 }
